fbe73decc1b2045c7c50dc522bbfe40b569b4282
febrefebril <andre.pnetto@gmail.com>
2015-04-12 21:55:46 -0300

    Todos erros de compilacao sanados.

--- james-bond-hat/src/james_bond_hat.ino
+++ james-bond-hat/src/james_bond_hat.ino
@@ -1,47 +1,46 @@
-// ################################################################################
-// #                    Projeto EuSonar - Modulo Bone                             #
-// #                    Autor: Andre Perotti Netto                                #
-// #                    Data criacao: 04/11/2015                                  #
-// ################################################################################
+// #####################################################################################
+// #                         Projeto EuSonar - Modulo Bone                             #
+// #                         Autor: Andre Perotti Netto                                #
+// #                         Data criacao: 04/11/2015                                  #
+// #####################################################################################
 // Prototipo do bone que visa evitar obstaculos
 
 #include<NewPing.h>
 // Constantes de configuracao geral
-#define DelayEntrePing 50
-#define DistanciaMediaBraco 50
-#define DistanciaMediaPasso 90
-#define Desligado 0
-
+#define DelayEntrePing              50// Tempo de espera entre as leituras ultrasom
+#define DistanciaMediaBraco         50// Distancia media braco humano
+#define DistanciaMediaPasso         90// Distancia media passo humano
+#define Desligado                    0
+#define DistanciaMaximaUltrasonico 300// Distancia maxima de leitura do ultrasom 3 m
+#define ForcaMediaMotor            128// TODO: medir a forca media e alterar
+#define ForcaAltaMotor             254// TODO: medir forca maxima do motor e alterar
 // Constantes que representam os motores de vibracao
-#define MotorFrontal  5
-#define MotorEsquerdo 6
-#define MotorDireito  7
-#define MotorTrazeiro 8
+#define MotorFrontal                 5// Pinos que estao ligados no motor frontal
+#define MotorEsquerdo                6// Pinos que estao ligados no motor esquerdo
+#define MotorDireito                 7// Pinos que estao ligados no motor direito
+#define MotorTrazeiro                8// Pinos que estao ligados no motor trazeiro
 
 // Constantes que representam os leds que ajudam a debugar
-#define LedFrontal    9
-#define LedEsquerdo   10
-#define LedDireito    11
-#define LedTrazeiro   12
-
-// Configuracao da distancia maxima dos sensores
-#define DistanciaMaximaUltrasom
+#define LedFrontal                   9// TODO: Estes leds ainda nao estao sendo usado
+#define LedEsquerdo                 10// TODO: Estes leds ainda nao estao sendo usado
+#define LedDireito                  11// TODO: Estes leds ainda nao estao sendo usado
+#define LedTrazeiro                 12// TODO: Estes leds ainda nao estao sendo usado
 
 // Constantes do sensor ultrasonico frontal
-#define UltrasonicoFrontalTrigger     13
-#define UltrasonicoFrontalEcho        14
+#define UltrasonicoFrontalTrigger   13// Numero do pino em que sera ligado o trigger
+#define UltrasonicoFrontalEcho      14// Numero do pino em que sera legado o echo
 
 // Constantes do sensor ultrasonico da lateral esquerda
-#define UltrasonicoEsquerdoTrigger    15
-#define UltrasonicoEsquerdoEcho       16
+#define UltrasonicoEsquerdoTrigger  15// Numero do pino em que sera ligado o trigger
+#define UltrasonicoEsquerdoEcho     16// Numero do pino em que sera legado o echo
 
 // Constantes do sensor ultrasonico da lateral direita
-#define UltrasonicoDireitoTrigger     17
-#define UltrasonicoDireitoEcho        18
+#define UltrasonicoDireitoTrigger   17// Numero do pino em que sera ligado o trigger
+#define UltrasonicoDireitoEcho      18// Numero do pino em que sera legado o echo
 
 // Constantes do sensor ultrasonico da lateral direita
-#define UltrasonicoDireitoTrigger     19
-#define UltrasonicoDireitoEcho        20
+#define UltrasonicoDireitoTrigger   19// Numero do pino em que sera ligado o trigger
+#define UltrasonicoDireitoEcho      20// Numero do pino em que sera legado o echo
 
 // Estrutura que controla em qual estado do programa estamos rodando
 enum EstadoDeExecucao
@@ -52,12 +51,12 @@ enum EstadoDeExecucao
     NavegacaoComMotoresLedSom,
     ControleRemotoComMotores,
     ControleRemotoComMotoresSom,
-    ControleRemotoComMotoresSomLed, 
+    ControleRemotoComMotoresSomLed,
     ReplayDeRotaSemAvisoDeObstaculo,
     ReplayDeRotaComAvisoDeObstaculoVibracao,
     ReplayDeRotaComAvisoDeObstaculoVibracaoSom,
     ReplayDeRotaComAvisoDeObstaculoVibracaoSomLed,
-    GravacaoRota, 
+    GravacaoRota,
     TesteDebug
 };
 
@@ -65,19 +64,19 @@ typedef enum EstadoDeExecucao estado;
 
 // inicia objetos que implemetam o sensores ultrasonico
 NewPing UltrasomFrontal(
-    UltrasonicoFrontalTrigger, 
+    UltrasonicoFrontalTrigger,
     UltrasonicoFrontalEcho,
     DistanciaMaximaUltrasonico
 );
 
 NewPing UltrasomLateralEsquerdo(
-    UltrasonicoEsquerdoTrigger, 
+    UltrasonicoEsquerdoTrigger,
     UltrasonicoEsquerdoEcho,
     DistanciaMaximaUltrasonico
 );
 
 NewPing UltrasomLateralDireito(
-    UltrasonicoDireitoTrigger, 
+    UltrasonicoDireitoTrigger,
     UltrasonicoDireitoEcho,
     DistanciaMaximaUltrasonico
 );
@@ -90,16 +89,18 @@ unsigned int DistanciaLateralEsquerdaAnterior;
 unsigned int DistanciaLateralDireita;
 unsigned int DistanciaLateralDireitaAnterior;
 unsigned int Duracao;
-unsigned int tempo;
+unsigned int Tempo;
 //  declara o estado inicial que representa o que o app
 //  esta fazendo no momento
-estado estado_atual_app; 
+estado estado_atual_app;
 
 
 // Retorna a diferenca do valor atual com valor anterior.
 int compara_distancias(unsigned int valor_atual, unsigned int valor_anterior);
 // Aciona ou para de acinar os motores de acordo com a comparacao das distancias.
 void trata_retorno_das_distancia_com_vibracao(int diferenca_das_distancias, String intencidade);
+// Desliga motor passado como parametro
+void desliga_motor(int numero_motor_desligar);
 void setup()
 {
     DistanciaFrontal = 0;
@@ -128,17 +129,13 @@ void setup()
 
 void loop()
 {
-
     // Independentemente do estado, sempre leio o ultrasom.
     scan();
-
-    // verifico em qual estado de execucao esta o app e chamo 
+    // verifico em qual estado de execucao esta o app e chamo
     // sua respectiva funcao
-
     switch( estado_atual_app )
     {
-
-        case EstadoIniciali :
+        case EstadoInicial :
             break;
         case NavegacaoComMotores :
             break;
@@ -165,9 +162,8 @@ void loop()
         case TesteDebug :
             break;
         default:
-
+            printf("Default atingido");
     }
-
 }
 
 // Faz a leitura de todos os sensores ultrasom e seta as variaveis com as distancias lidas.
@@ -176,20 +172,20 @@ void scan()
 
     DistanciaFrontalAnterior = DistanciaFrontal;
     DistanciaFrontal = Tempo = 0;
-    delay(DelayEntrePing); 
-    Tempo = UlstrasomFrontal.ping(); 
+    delay(DelayEntrePing);
+    Tempo = UltrasomFrontal.ping();
     DistanciaFrontal = Tempo / US_ROUNDTRIP_CM;
 
     DistanciaLateralEsquerdaAnterior = DistanciaFrontal;
     DistanciaLateralEsquerda = Tempo = 0;
-    delay(DelayEntrePing); 
-    Tempo = UlstrasomLateralEsquerdo.ping(); 
+    delay(DelayEntrePing);
+    Tempo = UltrasomLateralEsquerdo.ping();
     DistanciaLateralEsquerda = Tempo / US_ROUNDTRIP_CM;
 
     DistanciaLateralDireitaAnterior = DistanciaLateralDireita;
     DistanciaLateralDireita = Tempo = 0;
-    delay(DelayEntrePing); 
-    Tempo = UlstrasomLateralDireito.ping(); 
+    delay(DelayEntrePing);
+    Tempo = UltrasomLateralDireito.ping();
     DistanciaLateralDireita = Tempo / US_ROUNDTRIP_CM;
 
 }
@@ -197,12 +193,10 @@ void scan()
 // De acordo com as distancias lidas por scan() vibra os motores.
 void navegacao_com_motor()
 {
-
     // TODO: chamar as funcoes que tratam as distancias e vibrem os motores
     tratamento_motor("Frontal");
     tratamento_motor("Esquerdo");
     tratamento_motor("Direito");
-
 }
 
 void tratamento_motor(String posicao)
@@ -214,7 +208,7 @@ void tratamento_motor(String posicao)
     {
         distancia_medida = DistanciaFrontal;
     }
-    else if ( posicao == "Esquerdo" );
+    else if ( posicao == "Esquerdo" )
     {
         distancia_medida = DistanciaLateralEsquerda;
     }
@@ -222,12 +216,11 @@ void tratamento_motor(String posicao)
     {
         distancia_medida = DistanciaLateralDireita;
     }
-
-    if( distancia_medida > DistanciaMediaPasso ||  == Desligado )
+    if( distancia_medida > DistanciaMediaPasso || distancia_medida == Desligado )
     {
         // Distancia segura, desliga motores de vibracao.
         // TODO: fazer funcao que desligue os motores.
-        desliga_motor();
+        desliga_motor(posicao);
     }
     // Caso exista um objeto entre 50 e 90 cm de um aviso vibratorio medio.
     else if ( (distancia_medida > DistanciaMediaBraco || distancia_medida == Desligado) &&
@@ -235,126 +228,103 @@ void tratamento_motor(String posicao)
     {
         // TODO: chamar funcao que vibra com potencia media
         // Compara os valores para saber se o objeto esta se aproximando e da um alerta medio
-        trata_retorno_das_distancia_com_vibracao(comparacao_distancia, "MEDIA");
+        trata_retorno_das_distancia_com_vibracao(posicao, comparacao_distancia, "MEDIA");
     }
     // Caso em que o objeto se encontra em uma distancia menor que 50 cm, alerta com vibracao forte.
     else
     {
         // TODO: chamar funcao que vibra com potencia forte
-        trata_retorno_das_distancia_com_vibracao(comparacao_distancia, "FORTE");
+        trata_retorno_das_distancia_com_vibracao(posicao, comparacao_distancia, "FORTE");
     }
 }
 
-// Verifica distancias do ultrasom frontal e chama as funcoes de vibracao de acordo com a distancia.
-void tratamento_motor_frontal()
-{
-
-    // Verifa se a distancia aumentou ou diminuio.
-    int comparacao_distancia = compara_distancias(DistanciaFrontal, DistanciaFrontalAnterior);
-    // Caso nao haja nenhum objeto muito proximo, isto e, mais de 90 cm de distancia fontal.
-    if( DistanciaFrontal > DistanciaMediaPasso || DistanciaFrontal == Desligado )
-    {
-
-        // Distancia segura frontalmente, desliga motores de vibracao.
-        // TODO: fazer funcao que desligue os motores.
-        desliga_motor();
-
-    }
-    // Caso exista um objeto entre 50 e 90 cm de um aviso vibratorio medio.
-    else if ( (DistanciaFrontal > DistanciaMediaBraco || DistanciaFrontal == Desligado) &&
-              (DistanciaFrontal < DistanciaMediaPasso || DistanciaFrontal == Desligado) )
-    {
-
-        // TODO: chamar funcao que vibra com potencia media
-        // Compara os valores para saber se o objeto esta se aproximando
-        trata_retorno_das_distancia_com_vibracao(comparacao_distancia, "MEDIA");
-
-    }
-    // Caso em que o objeto se encontra em uma distancia menor que 50 cm, alerta com vibracao forte.
-    else
-    {
-
-        // TODO: chamar funcao que vibra com potencia forte
-        trata_retorno_das_distancia_com_vibracao(comparacao_distancia, "FORTE");
-
-    }
-
-}
-
-// Verifica distancias do ultrasom esquerdo e chama as funcoes de vibracao de acordo com a distancia.
-void tratamento_motor_esquerdo()
-{
-
-    // Verifa se a distancia aumentou ou diminuio.
-    int comparacao_distancia = compara_distancias(DistanciaLateralEsquerda, DistanciaLateralEsquerdaAnterior);
-    // Caso nao haja nenhum objeto muito proximo, isto e, mais de 90 cm de distancia lateral.
-    if( DistanciaLateralEsquerda > DistanciaMediaPasso || DistanciaLateralEsquerda == Desligado )
-    {
-
-        // Distancia segura lateral esquerda, desliga motores de vibracao.
-        // TODO: fazer funcao que desligue os motores.
-        desliga_motor();
-
-    }
-    // Caso exista um objeto entre 50 e 90 cm de um aviso vibratorio medio.
-    else if ( (DistanciaLateralEsquerda > DistanciaMediaBraco || DistanciaLateralEsquerda == Desligado) &&
-              (DistanciaLateralEsquerda < DistanciaMediaPasso || DistanciaLateralEsquerda == Desligado) )
-    {
-
-        // TODO: chamar funcao que vibra com potencia media
-        // Compara os valores para saber se o objeto esta se aproximando
-        trata_retorno_das_distancia_com_vibracao(comparacao_distancia, "MEDIA");
-
-    }
-    // Caso em que o objeto se encontra em uma distancia menor que 50 cm, alerta com vibracao forte.
-    else
-    {
-
-        // TODO: chamar funcao que vibra com potencia forte
-        trata_retorno_das_distancia_com_vibracao(comparacao_distancia, "FORTE");
-
-    }
-
-}
 // Retorna a diferenca do valor atual com valor anterior
 int compara_distancias(unsigned int valor_atual, unsigned int valor_anterior)
 {
-
-    return valor_atual - valor_anterior; 
-
+    return valor_atual - valor_anterior;
 }
 // Se a distancia esta diminuindo continua vibrando se a distancia se estabeliza ou aumenta, para.
-void trata_retorno_das_distancia_com_vibracao(int diferenca_das_distancias, String intencidade)
+void trata_retorno_das_distancia_com_vibracao(String posicao, int diferenca_das_distancias, String intencidade)
 {
-
         if( diferenca_das_distancias < 0 )
         {
-
             // Distancia esta diminuido, aumenta vibracao ou continua o aviso
             // TODO: fazer funcao que aumenta vibracao
             if( intencidade == "MEDIA")
             {
-
                 //TODO: Fazer funcao que liga o motor com intensidade media.
-                liga_motor("MEDIA"); // Fazer esta funcao.
-
+                liga_motor(posicao, ForcaMediaMotor); // Fazer esta funcao.
             }
             else if ( intencidade == "FORTE")
             {
-
                 //TODO: Fazer funcao que liga o motor com intensidade forte.
-                liga_motor("FORTE"); // Fazer esta funcao.
-
+                liga_motor(posicao, ForcaAltaMotor); // Fazer esta funcao.
             }
-
         }
         else
         {
-
             // Distancia se manteve ou esta aumento, diminui vibracao ou pare de vibrar
             // TODO: Fazer funcao que diminui vibracao ou deliga motor, por enquanto desligamos.
-            desliga_motor(); // Fazer esta funcao.
-
+            desliga_motor(posicao); // Fazer esta funcao.
         }
+}
+// Desliga Motor do pino passado como parametro posicao_motoresligar
+
+void desliga_motor(String posicao_motor)
+{
+    int numero_motor_desligar = 0;
+    if ( posicao_motor == "Frontal" )
+    {
+        numero_motor_desligar = MotorFrontal;
+    }
+    else if ( posicao_motor == "Esquedo" )
+    {
+        numero_motor_desligar = MotorEsquerdo;
+    }
+    else if ( posicao_motor == "Direito" )
+    {
+        numero_motor_desligar = MotorDireito;
+    }
+    else if ( posicao_motor == "Trazeiro" )
+    {
+        numero_motor_desligar = MotorTrazeiro;
+    }
+    else
+    {
+        numero_motor_desligar = 0;
+    }
+    if ( numero_motor_desligar )
+    {
+        analogWrite(numero_motor_desligar, 0);
+    }
+}
+// Liga motor 
+void liga_motor(String posicao_motor, int potencia)
+{
+    int numero_motor_ligar = 0;
+    if ( posicao_motor == "Frontal" )
+    {
+        numero_motor_ligar = MotorFrontal;
+    }
+    else if ( posicao_motor == "Esquedo" )
+    {
+        numero_motor_ligar = MotorEsquerdo;
+    }
+    else if ( posicao_motor == "Direito" )
+    {
+        numero_motor_ligar = MotorDireito;
+    }
+    else if ( posicao_motor == "Trazeiro" )
+    {
+        numero_motor_ligar = MotorTrazeiro;
+    }
+    else
+    {
+        numero_motor_ligar = 0;
+    }
+    if ( numero_motor_ligar )
+    {
+        analogWrite(numero_motor_ligar, potencia);
+    }
 
 }
