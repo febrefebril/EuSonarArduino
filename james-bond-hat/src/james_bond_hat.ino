d77b04812761e0e67687da54916dd32c5309c4eb
febrefebril <andre.pnetto@gmail.com>
2015-04-20 20:56:16 -0300

    Retatoramento, foi retirado todas as funcoes que nao estavam sendo usandas

--- james-bond-hat/src/james_bond_hat.ino
+++ james-bond-hat/src/james_bond_hat.ino
@@ -12,8 +12,8 @@
 #define DistanciaMediaPasso         90  // Distancia media passo humano
 #define Desligado                    0  
 #define DistanciaMaximaUltrasonico 300  // Distancia maxima de leitura do ultrasom 3 m
-#define ForcaMediaMotor            128  // TODO: medir a forca media e alterar
-#define ForcaAltaMotor             254  // TODO: medir forca maxima do motor e alterar
+#define ForcaMediaMotor             40  // TODO: medir a forca media e alterar
+#define ForcaAltaMotor              80  // TODO: medir forca maxima do motor e alterar
 // Constantes que definem o inicio, fim de comando
 #define InicioDeComando            '*'
 #define FimDeComando               '#'
@@ -22,12 +22,6 @@
 #define MotorEsquerdo               10  // Pinos que estao ligados no motor esquerdo
 #define MotorDireito                 9  // Pinos que estao ligados no motor direito
 #define MotorTrazeiro                6  // Pinos que estao ligados no motor trazeiro
-
-// Constantes que representam os leds que ajudam a debugar
-#define LedFrontal                  99  // TODO: Estes leds ainda nao estao sendo usado
-#define LedEsquerdo                 99  // TODO: Estes leds ainda nao estao sendo usado
-#define LedDireito                  99  // TODO: Estes leds ainda nao estao sendo usado
-#define LedTrazeiro                 99  // TODO: Estes leds ainda nao estao sendo usado
 
 // Constantes do sensor ultrasonico frontal
 #define UltrasonicoFrontalTrigger   12  // Numero do pino em que sera ligado o trigger
@@ -44,29 +38,11 @@
 #define QtdSonares                   3  // Quantidade de senssores ultra som
 #define IntervaloEntrePings         33  // Tempo em ms entre pings evitar echo cruzados 
 
-enum MotoresDeVibracao
-{
-    MotorDaFrente=1,
-    MotorDaEsquerda,
-    MotorDaDireita,
-    MotorDeTraz
-};
 // Estrutura que controla em qual estado do programa estamos rodando
 enum EstadoDeExecucao
 {
     MotoresDesligados=1,  // Motores desligados.
-    NavegacaoComMotores,
-    NavegacaoComMotoresLed,
-    NavegacaoComMotoresLedSom,
-    ControleRemotoComMotores,
-    ControleRemotoComMotoresSom,
-    ControleRemotoComMotoresSomLed,
-    ReplayDeRotaSemAvisoDeObstaculo,
-    ReplayDeRotaComAvisoDeObstaculoVibracao,
-    ReplayDeRotaComAvisoDeObstaculoVibracaoSom,
-    ReplayDeRotaComAvisoDeObstaculoVibracaoSomLed,
-    GravacaoRota,
-    TesteDebug
+    NavegacaoComMotores
 };
 
 typedef enum EstadoDeExecucao estado;
@@ -89,6 +65,7 @@ NewPing UltrasomLateralDireito(
     UltrasonicoDireitoEcho,
     DistanciaMaximaUltrasonico
 );
+
 //  Valores que conterao o tempo de espera de cada sensor
 //  ultrasom antes de uma nova leitura
 unsigned long CronometroPing[QtdSonares];
@@ -110,12 +87,6 @@ estado estado_atual_app;
 String string;
 char o_comando;
 
-// Retorna a diferenca do valor atual com valor anterior.
-int compara_distancias(unsigned int valor_atual, unsigned int valor_anterior);
-// Aciona ou para de acinar os motores de acordo com a comparacao das distancias.
-void trata_retorno_das_distancia_com_vibracao(int diferenca_das_distancias, String intencidade);
-// Desliga motor passado como parametro
-void desliga_motor(int numero_motor_desligar);
 void setup()
 {
 
@@ -138,20 +109,12 @@ void setup()
     DistanciaLateralDireitaAnterior = 0;
 
     estado_atual_app = NavegacaoComMotores;
-    // Descomente a linha de baixo para debugar
-    // Serial.begin(115200);
 
     // Inicia pinos dos motores como saida
     pinMode(MotorFrontal, OUTPUT);
     pinMode(MotorEsquerdo, OUTPUT);
     pinMode(MotorDireito, OUTPUT);
     pinMode(MotorTrazeiro, OUTPUT);
-
-    // Inicia pinos dos led como saida
-    pinMode(LedFrontal, OUTPUT);
-    pinMode(LedEsquerdo, OUTPUT);
-    pinMode(LedDireito, OUTPUT);
-    pinMode(LedTrazeiro, OUTPUT);
 }
 
 void loop()
@@ -164,7 +127,6 @@ void loop()
     }
     else
     {
-    
         if( Serial.available() > 0 )
         {
             string = "";
@@ -196,43 +158,12 @@ void loop()
             break;
         case NavegacaoComMotores :
             estado_navegacao_com_motores();
-            break;
-        case NavegacaoComMotoresLed :
-            esta_navegacao_com_motores_led();
-            break;
-        case NavegacaoComMotoresLedSom :
-            estado_navegacao_com_motores_led_som();
-            break;
-        case ControleRemotoComMotores :
-            estado_controle_remoto_com_motores();
-            break;
-        case ControleRemotoComMotoresSom :
-            estado_controle_remoto_com_motores_som();
-            break;
-        case ControleRemotoComMotoresSomLed :
-            estado_controle_remoto_com_motores_som_led();
-            break;
-        case ReplayDeRotaSemAvisoDeObstaculo :
-            estado_replay_de_rota_sem_aviso_de_obstaculo();
-            break;
-        case ReplayDeRotaComAvisoDeObstaculoVibracao :
-            estado_replay_de_rota_com_aviso_de_obstaculo_vibracao();
-            break;
-        case ReplayDeRotaComAvisoDeObstaculoVibracaoSom :
-            estado_replay_de_rota_com_aviso_de_obstaculo_vibracao_som();
-            break;
-        case ReplayDeRotaComAvisoDeObstaculoVibracaoSomLed :
-            estado_replay_de_rota_com_aviso_de_obstaculo_vibracao_som_led();
-            break;
-        case GravacaoRota :
-            estado_gravacao_rota();
-            break;
-        case TesteDebug :
-            estado_teste_debug();
-            break;
+
         default:
+            estado_navegacao_com_motores();
             Serial.println("Default atingido");
     }
+    
 }
 
 // Faz a leitura de todos os sensores ultrasom e seta as variaveis com as distancias lidas.
@@ -281,7 +212,7 @@ void scan()
         DistanciaLateralEsquerdaAnterior = DistanciaLateralEsquerda;
         DistanciaLateralEsquerda = Tempo = 0;// Limpa as duas variaveis antes de usar
         //delay(DelayEntrePing);
-        UltrasomLateralEsquerdo.timer_stop();
+        //UltrasomLateralEsquerdo.timer_stop();
         Tempo = UltrasomLateralEsquerdo.ping_median(5);
         DistanciaLateralEsquerda = Tempo / US_ROUNDTRIP_CM;
         Serial.print("Distancia Lateral Esquerda: ");
@@ -306,7 +237,7 @@ void scan()
         DistanciaLateralDireitaAnterior = DistanciaLateralDireita;
         DistanciaLateralDireita = Tempo = 0;// Limpa as duas variaveis antes de usar
         //delay(DelayEntrePing);
-        UltrasomLateralDireito.timer_stop();
+        //UltrasomLateralDireito.timer_stop();
         Tempo = UltrasomLateralDireito.ping_median(5);
         DistanciaLateralDireita = Tempo / US_ROUNDTRIP_CM;
         Serial.print("Distancia Lateral Direito: ");
@@ -319,28 +250,28 @@ void scan()
 void navegacao_com_motor()
 {
     // TODO: chamar as funcoes que tratam as distancias e vibrem os motores
-    tratamento_motor("Frontal");
-    tratamento_motor("Esquerdo");
-    tratamento_motor("Direito");
+    tratamento_motor(MotorFrontal);
+    tratamento_motor(MotorEsquerdo);
+    tratamento_motor(MotorDireito);
 }
 
-void tratamento_motor(String posicao)
+void tratamento_motor(int posicao)
 {
     Serial.println("Entrando em tratamento_motor()");
     int distancia_medida = 0; // vai receber a distancia de um dos sensores
     // Verifa se a distancia aumentou ou diminuio.
     int comparacao_distancia = compara_distancias(DistanciaFrontal, DistanciaFrontalAnterior);
-    if( posicao == "Frontal" )
+    if( posicao ==  MotorFrontal)
     {
         Serial.println("Trantando motor Frontal");
         distancia_medida = DistanciaFrontal;
     }
-    else if ( posicao == "Esquerdo" )
+    else if ( posicao == MotorEsquerdo )
     {
         Serial.println("Trantando motor Lateral esquerdo");
         distancia_medida = DistanciaLateralEsquerda;
     }
-    else if ( posicao == "Direito" )
+    else if ( posicao == MotorDireito )
     {
         Serial.println("Trantando motor Lateral Direito");
         distancia_medida = DistanciaLateralDireita;
@@ -363,7 +294,7 @@ void tratamento_motor(String posicao)
         Serial.print("Objeto proximo encontrado na posicao: ");
         Serial.print(posicao);
         Serial.println("");
-        trata_retorno_das_distancia_com_vibracao(posicao, comparacao_distancia, "MEDIA");
+        trata_retorno_das_distancia_com_vibracao(posicao, comparacao_distancia, ForcaMediaMotor);
     }
     // Caso em que o objeto se encontra em uma distancia menor que 50 cm, alerta com vibracao forte.
     else
@@ -372,8 +303,8 @@ void tratamento_motor(String posicao)
         Serial.print("ATENCAO: Objeto muito proximo ao sensor: ");
         Serial.print(posicao);
         Serial.println("");
-        trata_retorno_das_distancia_com_vibracao(posicao, comparacao_distancia, "FORTE");
-    }
+        trata_retorno_das_distancia_com_vibracao(posicao, comparacao_distancia, ForcaAltaMotor);
+    } 
 }
 
 // Retorna a diferenca do valor atual com valor anterior
@@ -382,18 +313,18 @@ int compara_distancias(unsigned int valor_atual, unsigned int valor_anterior)
     return valor_atual - valor_anterior;
 }
 // Se a distancia esta diminuindo continua vibrando se a distancia se estabeliza ou aumenta, para.
-void trata_retorno_das_distancia_com_vibracao(String posicao, int diferenca_das_distancias, String intencidade)
+void trata_retorno_das_distancia_com_vibracao(int posicao, int diferenca_das_distancias, int intencidade)
 {
         if( diferenca_das_distancias < 0 )
         {
             // Distancia esta diminuido, aumenta vibracao ou continua o aviso
             // TODO: fazer funcao que aumenta vibracao
-            if( intencidade == "MEDIA")
+            if( intencidade == ForcaMediaMotor)
             {
                 //TODO: Fazer funcao que liga o motor com intensidade media.
                 liga_motor(posicao, ForcaMediaMotor); // Fazer esta funcao.
             }
-            else if ( intencidade == "FORTE")
+            else if ( intencidade == ForcaAltaMotor)
             {
                 //TODO: Fazer funcao que liga o motor com intensidade forte.
                 liga_motor(posicao, ForcaAltaMotor); // Fazer esta funcao.
@@ -408,62 +339,14 @@ void trata_retorno_das_distancia_com_vibracao(String posicao, int diferenca_das_
 }
 // Desliga Motor do pino passado como parametro posicao_motoresligar
 
-void desliga_motor(String posicao_motor)
+void desliga_motor(int posicao_motor_desligar)
 {
-    int numero_motor_desligar = 0;
-    if ( posicao_motor == "Frontal" )
-    {
-        numero_motor_desligar = MotorFrontal;
-    }
-    else if ( posicao_motor == "Esquedo" )
-    {
-        numero_motor_desligar = MotorEsquerdo;
-    }
-    else if ( posicao_motor == "Direito" )
-    {
-        numero_motor_desligar = MotorDireito;
-    }
-    else if ( posicao_motor == "Trazeiro" )
-    {
-        numero_motor_desligar = MotorTrazeiro;
-    }
-    else
-    {
-        numero_motor_desligar = 0;
-    }
-    if ( numero_motor_desligar )
-    {
-        analogWrite(numero_motor_desligar, 0);
-    }
+    analogWrite(posicao_motor_desligar, 0);
 }
 // Liga motor 
-void liga_motor(String posicao_motor, int potencia)
+void liga_motor(int posicao_motor_alterar, int forca)
 {
-    int numero_motor_ligar = 0;
-    if ( posicao_motor == "Frontal" )
-    {
-        numero_motor_ligar = MotorFrontal;
-    }
-    else if ( posicao_motor == "Esquedo" )
-    {
-        numero_motor_ligar = MotorEsquerdo;
-    }
-    else if ( posicao_motor == "Direito" )
-    {
-        numero_motor_ligar = MotorDireito;
-    }
-    else if ( posicao_motor == "Trazeiro" )
-    {
-        numero_motor_ligar = MotorTrazeiro;
-    }
-    else
-    {
-        numero_motor_ligar = 0;
-    }
-    if ( numero_motor_ligar )
-    {
-        analogWrite(numero_motor_ligar, potencia);
-    }
+    analogWrite(posicao_motor_alterar, forca);
 
 }
 // Acao tomada caso nao esiver recebendo nada pelo bluetooth
@@ -486,66 +369,7 @@ void trata_comando_lido_pelo_bluetooth()
     {
         Serial.println("Estado: MotoresDesligados");
         estado_atual_app = MotoresDesligados;
-    }
-    else if(string == "NavegacaoComMotores")
-    {
-        Serial.println("Estado: NavegacaoComMotores");
-        estado_atual_app = NavegacaoComMotores; 
-    }
-    else if(string == "NavegacaoComMotoresLed")
-    {
-        Serial.println("Estado: NavegacaoComMotoresLed");
-        estado_atual_app = NavegacaoComMotoresLed;
-    }
-    else if(string == "NavegacaoComMotoresLedSom" )
-    {
-        Serial.println("Estado: NavegacaoComMotoresLedSom");
-        estado_atual_app = NavegacaoComMotoresLedSom;
-    }
-    else if(string == "ControleRemotoComMotores")
-    {
-        Serial.println("Estado: ControleRemotoComMotores");
-        estado_atual_app = ControleRemotoComMotores;
-    }
-    else if(string == "ControleRemotoComMotoresSom")
-    {
-        Serial.println("Estado: ControleRemotoComMotoresSom");
-        estado_atual_app = ControleRemotoComMotoresSom;
-    }
-    else if(string == "ControleRemotoComMotoresSomLed")
-    {
-        Serial.println("Estado: ControleRemotoComMotoresSomLed");
-        estado_atual_app = ControleRemotoComMotoresSomLed;
-    }
-    else if(string == "ReplayDeRotaSemAvisoDeObstaculo")
-    {
-        Serial.println("Estado: ReplayDeRotaSemAvisoDeObstaculo");
-        estado_atual_app = ReplayDeRotaSemAvisoDeObstaculo;
-    }
-    else if(string == "ReplayDeRotaComAvisoDeObstaculoVibracao")
-    {
-        Serial.println("Estado: ReplayDeRotaComAvisoDeObstaculoVibracao");
-        estado_atual_app = ReplayDeRotaComAvisoDeObstaculoVibracao;
-    }
-    else if(string == "ReplayDeRotaComAvisoDeObstaculoVibracaoSom")
-    {
-        Serial.println("Estado: ReplayDeRotaComAvisoDeObstaculoVibracaoSom");
-        estado_atual_app = ReplayDeRotaComAvisoDeObstaculoVibracaoSom;
-    }
-    else if(string == "ReplayDeRotaComAvisoDeObstaculoVibracaoSomLed")
-    {
-        Serial.println("Estado: ReplayDeRotaComAvisoDeObstaculoVibracaoSomLed");
-        estado_atual_app = ReplayDeRotaComAvisoDeObstaculoVibracaoSomLed;
-    }
-    else if(string == "GravacaoRota")
-    {
-        Serial.println("Estado: GravacaoRota");
-        estado_atual_app = GravacaoRota;
-    }
-    else if(string == "TesteDebug")
-    {
-        Serial.println("Estado: TesteDebug");
-        estado_atual_app = TesteDebug;
+        estado_motores_desligados();
     }
     else
     {  // Valor padrao.
@@ -560,53 +384,13 @@ void trata_comando_lido_pelo_bluetooth()
 void estado_motores_desligados()
 {
     Serial.println("estado_motores_desligados");
+    desliga_motor(MotorFrontal);
+    desliga_motor(MotorEsquerdo);
+    desliga_motor(MotorDireito);
+    desliga_motor(MotorTrazeiro);
 }
 void estado_navegacao_com_motores()
 {
     Serial.println("estado_navegacao_com_motore");
-
-}
-void esta_navegacao_com_motores_led()
-{
-    Serial.println("esta_navegacao_com_motores_led");
-}
-void estado_navegacao_com_motores_led_som()
-{
-    Serial.println("estado_navegacao_com_motores_led_som");
-}
-void estado_controle_remoto_com_motores()
-{
-    Serial.println("esta_controle_remoto_com_motores");
-}
-void estado_controle_remoto_com_motores_som()
-{
-    Serial.println("estado_controle_remoto_com_motores_som");
-}
-void estado_controle_remoto_com_motores_som_led()
-{
-    Serial.println("estado_controle_remoto_com_motores_som_led");
-}
-void estado_replay_de_rota_sem_aviso_de_obstaculo()
-{
-    Serial.println("estado_replay_de_rota_sem_aviso_de_obstaculo");
-}
-void estado_replay_de_rota_com_aviso_de_obstaculo_vibracao()
-{
-    Serial.println("estado_replay_de_rota_com_aviso_de_obstaculo_vibracao");
-}
-void estado_replay_de_rota_com_aviso_de_obstaculo_vibracao_som()
-{
-    Serial.println("estado_replay_de_rota_com_aviso_de_obstaculo_vibracao_som");
-}
-void estado_replay_de_rota_com_aviso_de_obstaculo_vibracao_som_led()
-{
-    Serial.println("estado_replay_de_rota_com_aviso_de_obstaculo_vibracao_som_led");
-}
-void estado_gravacao_rota()
-{
-    Serial.println("estado_gravacao_rota");
-}
-void estado_teste_debug()
-{
-    Serial.println("estado_teste_debug");
+    navegacao_com_motor();
 }
